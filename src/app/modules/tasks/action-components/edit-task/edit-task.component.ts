e092a275924d1e42802bdd0d8c3aee1b282cc7f9
George Chlaidze <george.chlaidze@gmail.com>
2024-02-27 22:33:59 +0400

    first commit

---
+++ src/app/modules/tasks/action-components/edit-task/edit-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-task.component.html',
+  styleUrl: './edit-task.component.css'
+})
+export class EditTaskComponent {
+
+}
---
+++ src/app/modules/tasks/tasks-list/tasks-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './tasks-list.component.html',
+  styleUrl: './tasks-list.component.css'
+})
+export class TasksListComponent {
+
+}
